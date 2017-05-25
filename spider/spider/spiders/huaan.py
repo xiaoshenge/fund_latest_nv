a8433f6645c9740201e1ec1d311ecdc0838afbbe
xiaoshenge <xiaoshengeer@gmail.com>
2017-05-26 01:23:16 +0800

    华安基金列表页

--- spider/spider/spiders/huaan.py
+++ spider/spider/spiders/huaan.py
@@ -5,7 +5,15 @@ import scrapy
 class HuaanSpider(scrapy.Spider):
     name = 'huaan'
     allowed_domains = ['huaan.com.cn']
-    start_urls = ['http://huaan.com.cn/']
+    start_urls = ['http://www.huaan.com.cn/supermarket/index.shtml']
 
     def parse(self, response):
+
+
+        fund_table = response.xpath('//table[@class="table_fund"]')
+        for i, v in enumerate(fund_table):
+            print v
         pass
+
+
+##基金超市 http://www.huaan.com.cn/fund/supermarket/load.do
