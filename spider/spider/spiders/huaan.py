65e6f7681859b4d39b30d064dafefd5980f2a0dd
xiaoshenge <xiaoshengeer@gmail.com>
2017-05-26 19:54:23 +0800

    华安数据完成

--- spider/spider/spiders/huaan.py
+++ spider/spider/spiders/huaan.py
@@ -1,19 +1,31 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 
 class HuaanSpider(scrapy.Spider):
     name = 'huaan'
     allowed_domains = ['huaan.com.cn']
-    start_urls = ['http://www.huaan.com.cn/supermarket/index.shtml']
+    start_urls = ['http://www.huaan.com.cn/fund/supermarket/load.do']
+
+    def getLatestNV(self, list, nvList):
+        for item in list:
+            nvList.append(dict(
+                fundId=item['fundCode'],
+                fundName=item['fundName'],
+                netValueDate=item['fundDate'],
+                netValue=item['fundValue'],
+                accumNetValue=item['fundTotalnetValue'],
+                unitNetChngPct=item['flucTuateper']))
+            if item['fundCode'] == '040035':
+                print item
+
 
     def parse(self, response):
+        jsonresponse = json.loads(response.body_as_unicode())
 
-
-        fund_table = response.xpath('//table[@class="table_fund"]')
-        for i, v in enumerate(fund_table):
-            print v
-        pass
-
-
-##基金超市 http://www.huaan.com.cn/fund/supermarket/load.do
+        nvList = []
+        self.getLatestNV(jsonresponse['fundinfoList3'], nvList)
